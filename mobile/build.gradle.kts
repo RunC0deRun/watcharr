10d272355b80e4f80242a665a5375c65e4a95c0b
Jan Stienstra <jan@jstienstra.nl>
2026-08-03 19:42:16 +0200

    Update version numbers

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.runc0derun.watcharr"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1005
-        versionName = "0.2.2"
+        versionCode = 1006
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.runc0derun.watcharr"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2005
-        versionName = "0.2.2"
+        versionCode = 2006
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
